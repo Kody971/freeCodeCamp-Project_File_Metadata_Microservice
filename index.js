1568a1195db4ba854518cb9f3f413edd82b918a7
Ayub Khoirudin <khoirudinayub971@gmail.com>
2024-11-28 16:13:35 +0700

    finished project

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+require("dotenv").config();
+var express = require("express");
+const multer = require("multer");
+var cors = require("cors");
 
 var app = express();
+const upload = multer({ storage: multer.memoryStorage() });
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+	res.sendFile(process.cwd() + "/views/index.html");
 });
 
-
-
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res, next) => {
+	const file = req.file;
+	res.json({
+		name: file.originalname,
+		type: file.mimetype,
+		size: file.size,
+	});
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+	console.log("Your app is listening on port " + port);
 });
